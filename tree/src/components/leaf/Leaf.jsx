71fbf74acb531ee60eefedfa93b75052aa58952e
Sofya Sidonskaya <sonyasidonya@gmail.com>
2025-04-09 18:02:52 +0700

    Leaves
    made a leaves component and added them to the tree

--- tree/src/components/leaf/Leaf.jsx
+++ tree/src/components/leaf/Leaf.jsx
@@ -2,11 +2,34 @@ import React, {useState} from 'react';
 
 import './leaf.css';
 
-const Leaf = () => {
-    return (
-        <div>
+const Leaf = ({name, childrenLeaves}) => {
+    const [children, setChildren] = useState(childrenLeaves);
+    const [leafName, setLeafName] = useState(name);
+    const [chosen, setChosen] = useState(false);
 
+    const handleNameChange = (event) => {
+        setLeafName(event.target.value); 
+    };
+
+    const handleChosen = () => {
+        setChosen(!chosen);
+    }
+
+    return (
+        <>
+        <div className={chosen ? "leaf chosen" : "leaf"} onClick={handleChosen}>
+            <input
+                type="text"
+                value={leafName}
+                onChange={handleNameChange}
+                disabled/>
         </div>
+        <div className="children-div">
+            {children.map(({id, name, children}) => (
+                <Leaf key={id} name={name} childrenLeaves={children}/>
+            ))}
+        </div>
+        </>
     );
 }
 
--- tree/src/components/tree/Tree.jsx
+++ tree/src/components/tree/Tree.jsx
@@ -2,8 +2,20 @@ import React, {useState} from 'react';
 
 import './tree.css';
 import Button from '../button/button';
+import Leaf from '../leaf/Leaf';
+
+const baseLeaves = [
+    {
+        id: 1, name: "Node 1", children:[{id: 3, name: "Node 3", children: []}]
+    },
+    {
+        id: 2, name: "Node 2", children: []
+    }
+]
 
 const Tree = () => {
+    const [leaves, setLeaves] = useState(baseLeaves);
+    const [chosenLeaf, setChosenLeaf] = useState();
     return (
         <div className='tree-div'>
             <div className="buttons-div">
@@ -15,7 +27,9 @@ const Tree = () => {
             <div className='right-div'>
                 <h1 className='title'>tree</h1>
                 <div className='tree-container'>
-                    
+                    {leaves.map(({id, name, children}) => (
+                        <Leaf key={id} name={name} childrenLeaves={children}/>
+                    ))}
                 </div>
             </div>
         </div>
